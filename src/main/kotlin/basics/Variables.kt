2e3eda4fdbd34315ab5ef7b9411cf86bad900924
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-19 14:55:02 +0530

    variables

--- src/main/kotlin/basics/Variables.kt
+++ src/main/kotlin/basics/Variables.kt
@@ -26,7 +26,30 @@ fun main(args: Array<String>) {
     val myInt = 20
     val myLongg = myInt // Treated as int, value stored as int, as type is inferred as int
 
-    val myLonggg:Long  = myInt // Type mismatch, not implicit conversion
+    // val myLonggg: Long = myInt // Type mismatch, not implicit conversion
 
-    val myLonggggg:Long = myInt.toLong() // Explicit conversion
+    val myLonggggg: Long = myInt.toLong() // Explicit conversion
+
+
+    // String
+    val char = 'A'
+    val string = "This is string"
+    val escapeCharacter = "With new line \n"
+
+    val rawString = "This is line 1 " +
+            "Line two " +
+            "Line 3"
+    val multipleLines = """
+        This is an another way, of writing in multiple lines
+        This is second line
+        This is third line
+        """
+
+    // String interpolation
+    val year = 10
+    val yearStr = "Year is $year"
+
+    val fName = "Ritesh"
+    val nameLength = "Name length is ${fName.length}"
+
 }
