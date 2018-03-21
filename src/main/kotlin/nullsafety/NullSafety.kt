cc1e32ac6605b979cb0a60a4abee2cb57cd2faac
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-21 10:16:42 +0530

    null safety

---
+++ src/main/kotlin/nullsafety/NullSafety.kt
@@ -0,0 +1,22 @@
+package nullsafety
+
+/**
+ * By default kotlin in null safe
+ */
+
+fun main(args: Array<String>) {
+    val message: String = "A String"
+    println(message.length)
+
+    // Null safe - for eg, types cannot be null  by default
+    //val msg:String = null
+
+    // To, work with null type
+    val nullMessage: String? = null
+    val inferredNull = null
+
+//    println(nullMessage.length)
+    println(nullMessage?.length)
+
+    println(nullMessage!!.length) // throw exception
+}
