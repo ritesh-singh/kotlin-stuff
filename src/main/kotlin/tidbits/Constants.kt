159552c7d48f91014389acf280e0c600be0eae28
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-22 12:36:54 +0530

    constants

---
+++ src/main/kotlin/tidbits/Constants.kt
@@ -0,0 +1,12 @@
+package tidbits
+
+val CopyrightAuthor = "Hadi Hariri"
+
+object Copyright {
+    val author = "Hadi Hariri"
+}
+
+fun main(args: Array<String>) {
+    println(Copyright.author)
+    println(CopyrightAuthor)
+}
