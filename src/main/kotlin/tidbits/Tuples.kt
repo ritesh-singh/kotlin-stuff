5f6679bf4b4816e634b3912a26facc317e9efa6a
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-22 12:31:50 +0530

    exceptions

--- src/main/kotlin/tidbits/Tuples.kt
+++ src/main/kotlin/tidbits/Tuples.kt
@@ -1,5 +1,7 @@
 package tidbits
 
+import classes.CustomerKotlin
+
 fun capitalAndPopulation(country: String): Pair<String, Long> {
     return Pair("Madrid", 2300000)
 }
@@ -12,6 +14,37 @@ fun main(args: Array<String>) {
     val result = capitalAndPopulation("Madrid")
     result.first
 
+
     val countryInfo = countryInformation("Madrid")
     countryInfo.third
+
+
+    println("-----------------------------------")
+
+    // Destructuring
+
+
+    // Calls component1, component2 and component3
+    // Components signified the position
+    val (capital, continent, population) = countryInformation("Madrid")
+    println(capital)
+    println(continent)
+    println(population)
+
+    println("-------------------------------------")
+
+    // Destructuring data classes
+    val (id, name, email) = CustomerKotlin(1, "Max", "max@gmail.com")
+    println(id)
+    println(name)
+    println(email)
+
+    println("-------------------------------------")
+
+    // Destructuring in for loops
+    val listCapitalsAndCounties = listOf(Pair("Madrid", "Spain"), "Paris" to "France")
+    for ((capital, country) in listCapitalsAndCounties) {
+        println("$capital - $country")
+    }
+
 }
