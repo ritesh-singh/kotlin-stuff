21549c3ca3209b0a86c28a11b521f740ea502887
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-25 16:55:26 +0530

    infix notation

---
+++ src/main/kotlin/advanced/functions/InfixNotations.kt
@@ -0,0 +1,22 @@
+package advanced.functions
+
+/**
+ * In case of extension functions or member functions, that have single parameter, can be called using
+ * infix notations.
+ */
+
+
+/**
+ * It's much more readable and expressive way.
+ */
+
+fun String.shouldBeEqualTo(value: String) = this == value
+
+infix fun String.isMatching(value: String) = this == value
+
+fun main(args: Array<String>) {
+
+    println("Hello".shouldBeEqualTo("Hello"))
+
+    println("Hello" isMatching "Hello")
+}
