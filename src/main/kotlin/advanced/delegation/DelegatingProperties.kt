61e0b3a01935f3195752f8b12ce586ba200fab83
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-28 21:10:41 +0530

    Delegating properties

--- src/main/kotlin/advanced/delegation/DelegatingProperties.kt
+++ src/main/kotlin/advanced/delegation/DelegatingProperties.kt
@@ -1,2 +1,33 @@
 package advanced.delegation
 
+import kotlin.reflect.KProperty
+
+class Service {
+    var someProperty: String by ExternalFunctionality()
+}
+
+class ExternalFunctionality {
+
+    // No default backing field, so let's create one
+    var backingField = "Default"
+
+    operator fun getValue(service: Service, property: KProperty<*>): String {
+        println("getValue called with params: \n" +
+                "service: $service\n" +
+                "property:${property.name}")
+        return backingField
+    }
+
+    operator fun setValue(service: Service, property: KProperty<*>, value: String) {
+        backingField = value
+    }
+
+}
+
+fun main(args: Array<String>) {
+    val service = Service()
+
+    println(service.someProperty)
+    service.someProperty = "New Value"
+    println(service.someProperty)
+}
