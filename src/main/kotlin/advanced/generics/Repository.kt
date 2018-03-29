45d600875360b904acc7df30503148095667d3c6
Ritesh K Singh <riteshdvps10@gmail.com>
2018-03-29 11:46:46 +0530

    generic with upper bound

---
+++ src/main/kotlin/advanced/generics/Repository.kt
@@ -0,0 +1,39 @@
+package advanced.generics
+
+import java.io.Serializable
+
+// Upper Bound Restriction on Generics
+
+open class Entity(val id: Int)
+
+
+// Upper bound with one restriction
+class Repository<T : advanced.generics.Entity> {
+    fun save(entity: T) {
+        if (entity.id != 0) {
+            // ....
+        }
+    }
+}
+
+// Upper bound with many restrictions
+class Database<T> where T : advanced.generics.Entity, T : Serializable {
+    fun save(entity: T) {
+        if (entity.id != 0) {
+            // ....
+        }
+    }
+}
+
+// Function with upper bound restriction
+fun <T : Serializable> streamObject(obj: T) {
+
+}
+
+class CustomerEntity(id: Int) : advanced.generics.Entity(id) {
+
+}
+
+fun main(args: Array<String>) {
+    val repo = Repository<CustomerEntity>()
+}
