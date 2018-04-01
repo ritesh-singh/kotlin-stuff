3e8983dd418b480d919d3451a54a00c74bf4ef40
Ritesh K Singh <riteshdvps10@gmail.com>
2018-04-01 22:28:22 +0530

    Using Java reflection api in kotlin

---
+++ src/main/kotlin/advanced/metaprogramming/Transaction.kt
@@ -0,0 +1,37 @@
+package advanced.metaprogramming
+
+import java.lang.reflect.Type
+
+class Transaction(val id: Int, val amount: Double, var desc: String) {
+    fun validate() {
+        if (amount > 10000) {
+            println("advanced.metaprogramming.Transaction is too large")
+        }
+    }
+}
+
+
+// Using JavaAPI
+fun interospectInstance(obj: Any) {
+    println("Class ${obj.javaClass.simpleName}")
+    println("Properties\n")
+    obj.javaClass.declaredFields.forEach {
+        println("${it.name} of type ${it.type}")
+    }
+    println("Functions\n")
+    obj.javaClass.declaredMethods.forEach {
+        println("${it.name}")
+    }
+
+}
+
+fun getType(obj: Type){
+    println(obj.typeName)
+}
+
+fun main(args: Array<String>) {
+//    interospectInstance(Transaction(1, 200.0, "A simple transaction"))
+
+    getType(Transaction::class.java)
+}
+
