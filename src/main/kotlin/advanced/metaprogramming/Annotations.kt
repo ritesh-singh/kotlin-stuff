b306ee7d2405fb57aa075715d712f5047b5e149c
Ritesh K Singh <riteshdvps10@gmail.com>
2018-04-02 09:10:21 +0530

    Annotations

---
+++ src/main/kotlin/advanced/metaprogramming/Annotations.kt
@@ -0,0 +1,18 @@
+package advanced.metaprogramming
+
+@Target(AnnotationTarget.CLASS)
+annotation class Table(val name: String)
+
+@Target(AnnotationTarget.PROPERTY)
+annotation class Field(val name: String)
+
+
+@Table(name = "ContactTable")
+data class Contact(val id: Int, @Field( name = "NAME") val name: String, val email: String)
+
+fun main(args: Array<String>) {
+
+    val annotation = Contact::class.annotations.find { it.annotationClass.simpleName == "Table" }
+
+    println(annotation)
+}
