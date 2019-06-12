4fab3db6cb3758d7b320c4ce4a465c8a1a53af68
Ritesh <ritesh.singh@target.com>
2019-06-13 01:12:05 +0530

    Sealed class

---
+++ src/main/kotlin/sealedclass/SealedClass.kt
@@ -0,0 +1,15 @@
+package sealedclass
+
+fun main(args: Array<String>) {
+    fun speed(car: Car): Int = when (car) {
+        is Car.Maruti -> car.speed
+        is Car.Bugatti -> car.speed + car.boost
+        Car.NotACar -> -1
+    }
+}
+
+sealed class Car {
+    data class Maruti(val speed: Int) : Car()
+    data class Bugatti(val speed: Int, val boost: Int) : Car()
+    object NotACar : Car() // Objdct
+}
